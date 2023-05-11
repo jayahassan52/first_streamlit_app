3be7f05f4bb250cf75e126799a9227c56b76412f
jayahassan52 <125984421+jayahassan52@users.noreply.github.com>
2023-05-11 17:31:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 
 
 streamlit.title('My Parents New Healthy Dinner')
@@ -13,5 +13,5 @@ streamlit.dataframe(my_fruit_list)
 
 
 
-
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
