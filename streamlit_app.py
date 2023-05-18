8839d0f5299c21382bdcd0f2c9e7c02824aa9584
jayahassan52 <125984421+jayahassan52@users.noreply.github.com>
2023-05-18 20:22:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,18 +26,21 @@ streamlit.dataframe(my_fruit_list)
 
 ###
 #create the repeatable code block (called a function)
-def get fruityvice data(this fruit choice):
-fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json ())
-return fruityvice normalized
-'streamlit.header( 'Fruityvice Fruit Advice!')
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+st.header('Fruityvice Fruit Advice!')
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?")
-if not fruit choice:
-streamlit.error("Please select a fruit to get information.'
-else:
-back_from_function = get_fruityvice_data(fruit_choice)
-streamlit.dataframe(back_from_function)
+    fruit_choice = st.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        st.error("Please select a fruit to get information.")
+    else:
+        back_from_function = get_fruityvice_data(fruit_choice)
+        st.dataframe(back_from_function)
+except requests.exceptions.RequestException as e:
+    st.error("An error occurred while fetching the fruit information.")
   
 
 
