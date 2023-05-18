e64819e0c02ab5ec13e0c86a09dc52fd7bbb60aa
jayahassan52 <125984421+jayahassan52@users.noreply.github.com>
2023-05-18 14:45:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+pip install "snowflake-snowpark-python[pandas]"
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
